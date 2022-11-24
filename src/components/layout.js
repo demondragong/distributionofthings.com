edf5b37afae8932303c0873cf431cbb187b25029
Gael <demondragong@gmail.com>
2022-11-24 15:43:29 +0100

    add newsletter subscription in footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,35 @@ const Layout = ({ location, children }) => {
         <Link to="/about">about</Link>
       </header>
       <main>{children}</main>
-      <footer></footer>
+      <footer>
+        <div className="newsletter-container">
+          <form method="post" action="/subscription">
+            <input
+              type="hidden"
+              name="csrfmiddlewaretoken"
+              defaultValue="MnzODHNSJLaH8YFW6xsAkr3nsKO6mTwoqrywoekqMuoUU31Ujg50PUTLdkUPd5OL"
+            />
+            <div className="row align-items-center">
+              <label htmlFor="newsletter" className="col-sm-8 my-2">
+                Receive an email when a new distribution is published. About
+                once per month.
+              </label>
+              <div className="newsletter-input-container">
+                <input
+                  id="newsletter"
+                  type="email"
+                  name="email"
+                  className="form-control"
+                  placeholder="Email address"
+                />
+                <button className="btn btn-outline-secondary" type="submit">
+                  Subscribe
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </footer>
     </div>
   )
 }
