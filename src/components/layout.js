c85cf290d8a6b3d1288ab6de285ea73e520c4cc9
Gael <demondragong@gmail.com>
2022-12-23 23:18:01 +0100

    fix newsletter signup

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,31 +13,70 @@ const Layout = ({ location, children }) => {
       </header>
       <main>{children}</main>
       <footer>
-        <div className="newsletter-container">
-          <form method="post" action="/subscription">
-            <input
-              type="hidden"
-              name="csrfmiddlewaretoken"
-              defaultValue="MnzODHNSJLaH8YFW6xsAkr3nsKO6mTwoqrywoekqMuoUU31Ujg50PUTLdkUPd5OL"
-            />
-            <div className="row align-items-center">
-              <label htmlFor="newsletter" className="col-sm-8 my-2">
-                Receive an email when a new distribution is published. Sometimes.
-              </label>
-              <div className="newsletter-input-container">
-                <input
-                  id="newsletter"
-                  type="email"
-                  name="email"
-                  className="form-control"
-                  placeholder="Email address"
-                />
-                <button className="btn btn-outline-secondary" type="submit">
-                  Subscribe
-                </button>
+        <div className="signup-container">
+          <div>Subscribe to receive the next distribution. Unsubscribe anytime.</div>
+          <div id="mc_embed_signup">
+            <form
+              action="https://distributionofthings.us20.list-manage.com/subscribe/post?u=e2c3523040011f23db3a10671&id=4da03abbc6&f_id=00df55e6f0"
+              method="post"
+              id="mc-embedded-subscribe-form"
+              name="mc-embedded-subscribe-form"
+              className="validate"
+              target="_blank"
+              noValidate=""
+            >
+              <div id="mc_embed_signup_scroll">
+                <div className="mc-field-group">
+                  <label className="signup-email-label" htmlFor="mce-EMAIL">Email Address</label>
+                  <input
+                    type="email"
+                    defaultValue=""
+                    name="EMAIL"
+                    className="required email signup-email-input"
+                    id="mce-EMAIL"
+                    required
+                    placeholder="Email Address"
+                  />
+                  <span id="mce-EMAIL-HELPERTEXT" className="helper_text" />
+                </div>
+                <div id="mce-responses" className="clear foot">
+                  <div
+                    className="response"
+                    id="mce-error-response"
+                    style={{ display: "none" }}
+                  />
+                  <div
+                    className="response"
+                    id="mce-success-response"
+                    style={{ display: "none" }}
+                  />
+                </div>
+                {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups*/}
+                <div
+                  style={{ position: "absolute", left: "-5000px" }}
+                  aria-hidden="true"
+                >
+                  <input
+                    type="text"
+                    name="b_e2c3523040011f23db3a10671_4da03abbc6"
+                    tabIndex={-1}
+                    defaultValue=""
+                  />
+                </div>
+                <div className="optionalParent">
+                  <div className="clear foot">
+                    <input
+                      type="submit"
+                      value="Subscribe"
+                      name="subscribe"
+                      id="mc-embedded-subscribe"
+                      className="button signup-email-button"
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </footer>
     </div>
