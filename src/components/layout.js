b6f706b66254359eed376fc60fd8e750591aaae6
Gaël <demondragong@gmail.com>
2023-04-04 10:44:02 +0200

    change footer link to twitter

--- src/components/layout.js
+++ src/components/layout.js
@@ -83,7 +83,7 @@ const Layout = ({ location, children }) => {
           </div>
         </div>
         <div className="copyright-container">
-          <a href="https://gaël.com">Gaël</a> © 2023
+          <a href="https://twitter.com/gaeldemondragon">Gaël</a> © 2023
         </div>
       </footer>
     </div>
